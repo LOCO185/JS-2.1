d9ef6d37dd4e8f730c00643168cb9c88f3b092bb
LOCO185 <jloco151@gmail.com>
2021-11-07 20:34:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function describeCountry(country, population, capitalCity){
+    return `${country} has ${population} people and its capital is ${capitalCity}`;
+
+}
+console.log(describeCountry('finland','6 million', 'helsinki.'));
+console.log(describeCountry('israel','7 million', 'jerusalem.'));
+console.log(describeCountry('USA','140 million', 'Washington DC.'));
+
+
+
+
